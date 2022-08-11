05fb99054f65bb1aa24b3b7b276c055c3197da57
Oleksandr <Pidvysotskyi.oleksandr@gmail.com>
2022-08-11 11:57:32 +0300

    main done

--- src/index.js
+++ src/index.js
@@ -1,20 +1,57 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import ApiCountry from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
 
-import ApiCountry from './fetchCountries';
-
 const inputRef = document.querySelector('#search-box');
+const countryListRef = document.querySelector('.country-list');
+const countryInfoRef = document.querySelector('.country-info');
 const newApiCountry = new ApiCountry();
 
 inputRef.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
 function onInput(evt) {
   newApiCountry.query = evt.target.value.trim();
+  countryListRef.innerHTML = '';
+  countryInfoRef.innerHTML = '';
+
   if (newApiCountry.query !== '') {
     newApiCountry.fetchCountries().then(result => {
-      console.log(result.length);
+      if (result.length > 10) {
+        Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      } else if (result.length < 10 && result.length >= 2) {
+        countryListRef.insertAdjacentHTML('afterbegin', listMarkup(result));
+      } else {
+        countryInfoRef.insertAdjacentHTML('afterbegin', cardMarkup(result));
+      }
     });
   }
+}
+
+function cardMarkup(countries) {
+  const cardMarkup = countries
+    .map(i => {
+      return `<div class='header'>
+      <img width="20px" src='${i.flags.svg}' alt='${i.name.common} flag' />
+      <b>${i.name.official}</b></div>
+      <p><b>Capital:</b>${i.capital}</p>
+      <p><b>Population:</b>${i.population}</p>
+      <p><b>Languages:</b>${Object.values(i.languages)}</p>`;
+    })
+    .join('');
+  return cardMarkup;
+}
+function listMarkup(countries) {
+  const listMarkup = countries
+    .map(i => {
+      return `<li><div class="list-item">
+    <img width="20px" src="${i.flags.svg}" alt="${i.name.common} flag" />
+    <span>${i.name.common}</span></div></li>`;
+    })
+    .join('');
+  return listMarkup;
 }
